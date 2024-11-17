88e05907756df527767448b56a4981bb1521cd85
Patryk <patryk.michno.contact@gmail.com>
2024-11-17 21:07:26 +0100

    added apt bese system

--- ansible_preparation_with_user.sh
+++ ansible_preparation_with_user.sh
@@ -2,8 +2,18 @@
 
 # Ensure the script is run as root
 if [[ $EUID -ne 0 ]]; then
-   echo "This script must be run as root." 
+   echo "This script must be run as root."
    exit 1
+fi
+
+# Detect package manager
+if grep -q "ID_LIKE=.*rhel" /etc/os-release; then
+    PM="dnf"
+elif grep -q "ID_LIKE=.*debian" /etc/os-release; then
+    PM="apt"
+else
+    echo "Unsupported OS. Exiting."
+    exit 1
 fi
 
 # Prompt for the username and password
@@ -17,7 +27,11 @@ echo "$username:$user_password" | chpasswd
 echo "User $username created successfully."
 
 # Add the user to the sudo group
-usermod -aG wheel "$username"
+if [[ $PM == "apt" ]]; then
+    usermod -aG sudo "$username"
+else
+    usermod -aG wheel "$username"
+fi
 echo "User $username added to the sudo group."
 
 # Set up SSH directory and keys
@@ -44,20 +58,24 @@ chmod 600 "$ssh_config"
 chown "$username:$username" "$ssh_config"
 echo "SSH configuration for GitLab created successfully."
 
-# Update system and install Git
+# Update the system and install Git
 echo "Updating the system and installing Git..."
-dnf update -y && dnf install -y git
+if [[ $PM == "dnf" ]]; then
+    dnf update -y && dnf install -y git
+else
+    apt update && apt upgrade -y && apt install -y git
+fi
 echo "Git installed successfully."
 
-# Install Ansible using pip
+# Install pip
 echo "Installing pip..."
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 rm -f get-pip.py
 
+# Install Ansible for the created user
 echo "Installing Ansible for $username..."
-sudo -u "$username" python3 -m pip install --user "$username"
+sudo -u "$username" python3 -m pip install -user ansible
 echo "Ansible installed successfully for $username."
 
 echo "Script execution completed!"
-
